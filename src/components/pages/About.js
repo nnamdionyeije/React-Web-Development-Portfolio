3a64ed40f5c35e6f258beb4da3cd14ad867b863e
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-09-12 21:54:54 -0400

    fixing deployment issues
    -- adding homepage line

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,7 +8,7 @@ function About() {
             <img src='https://media.discordapp.net/attachments/540526756545691660/1119100108148060270/pic_of_me.jpg'
             width={200}></img>
 
-            <p>Officia do irure fugiat ea ex labore adipisicing sunt ut non nulla incididunt id. Voluptate quis aliquip ipsum labore est. Eu quis duis aliqua esse. Sit magna cillum anim ea cillum. Elit magna proident sint commodo excepteur qui. Reprehenderit non dolore mollit et reprehenderit ex officia laboris ad laboris. Enim exercitation aliquip id pariatur exercitation labore adipisicing ullamco cupidatat proident.</p>
+            <p>I am a full-stack web developer and senior computer science student at the Georgia Tech Institute of Technology. My experience with student politics allows me to have a unique approach to facilitating client communication and working towards quick resolutions on any large project I lead. My primary competencies are in MVC and MERN structured applications. </p>
         </div >    
     
     );
